3924b1bc7da8d28bdefdcd2e5e839370ee61829f
Javed <jay7134@gmail.com>
2022-06-17 18:56:24 +0500

    Home Page

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,21 @@
 import React, {useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
+import $ from 'jquery';
 function Home(){
+    useEffect(() => {
+        setTimeout(() => {
+            $("#RH-entry-video").addClass("d-none");
+            $(".RH-loop-entry-video").removeClass("d-none");
+        }, 8000);
+    }, []); 
     return(
         <>
-        
         <img id="bg_img" className='img-fluid d-none animate__animated' src='/images/bg-1.jpg' alt='retro hunters bg' width={1920} height={1080} />
         <video id="RH-entry-video" className="RH-entry-video" autoPlay muted>
-            <source src="/videos/RH-home-entry.mp4" type='video/mp4' />
+            <source src="/videos/RH_Site_Entry.mp4" type='video/mp4' />
         </video>
-        <video id="RH-entry-join" className="RH-entry-video d-none" autoPlay loop muted>
-            <source src="/videos/RH-home-1.mp4" type='video/mp4' />
+        <video id="RH-entry-video-2" className="RH-loop-entry-video d-none" autoPlay loop muted>
+            <source src="/videos/Looping-arcade-machine.mp4" type='video/mp4' />
         </video>
         <section id="Home-page" className="rh-home">
             <div className="container">
